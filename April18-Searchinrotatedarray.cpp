8e0b294858403178782bef1e46d232df1803a3b7
Yoel Yonata <yoel.yonata@gmail.com>
2020-04-20 23:46:04 -0700

    Completed april 18 challenge

---
+++ April18-Searchinrotatedarray.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        int mid = 0;
+        while (l<=r){
+            mid = (l+r)/2;
+            if (target == nums[mid]){
+                return mid;
+            }
+            if(nums[mid] > nums[r]){
+                if (target <nums[mid] && target>= nums[l]){
+                    r = mid -1;
+                }
+                else{
+                    l = mid+1;
+                }  
+            }
+            else if(nums[mid]< nums[l]){
+                if (target >nums[mid] && target <= nums[r]){
+                    l = mid+1;
+                }
+                else{
+                    r = mid -1;
+                }  
+            }
+            else{
+                if(target <nums[mid]){
+                    r = mid - 1;
+                }else{
+                    l = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+};
