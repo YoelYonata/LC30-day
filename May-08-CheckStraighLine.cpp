385ca08606d2a3d62c8e3b50da0097b20c1879ca
Yoel Yonata <yoel.yonata@gmail.com>
2020-05-08 23:14:15 -0700

    Finish May 8 challenge

---
+++ May-08-CheckStraighLine.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        double initialSlope;
+        double temp;
+        if (coordinates.size() <= 2){
+            return true;
+        }
+        initialSlope = ((double)coordinates[1][1] - coordinates[0][1])/(coordinates[1][0] - coordinates[0][0]);
+        for (int i = 1; i<coordinates.size(); i++){
+            temp = ((double)coordinates[i][1] - coordinates[i-1][1])/(coordinates[i][0] - coordinates[i-1][0]);
+            if (temp != initialSlope ){
+                return false;
+            }
+            initialSlope = temp;
+        }
+        return true;
+    }
+};
